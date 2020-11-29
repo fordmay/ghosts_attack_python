cb956918050c3998a98ef7cd7fddfaac6b777c68
Dmytro <d.u.zheliabin@gmail.com>
2020-11-29 20:42:17 +0200

    Reduce game update to FPS(60)

--- ghosts_attack.py
+++ ghosts_attack.py
@@ -8,12 +8,18 @@ class GhostsAttack:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((1200, 600))
         pygame.display.set_caption("Ghosts Attack")
+        # Variables to synchronize refresh the game and FPS.
+        self.FPS = 60
+        self.clock = pygame.time.Clock()
 
     def run_game(self):
         """Start the main loop for the game."""
+
         while True:
+            # Reduce game update to frames per second. Save PC resource
+            self.clock.tick(self.FPS)
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
